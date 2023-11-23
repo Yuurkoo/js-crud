847747da86432dd788766d0adee3c2860400d0b7
Yuurkoo <managerw52@gmail.com>
2023-11-23 23:33:43 +0200

    first commit

--- src/route/index.js
+++ src/route/index.js
@@ -7,45 +7,138 @@ const router = express.Router()
 class Product {
   static #list = []
 
-  constructor(name, description, price) {
-    this.name = name
+  static #count = 0
+
+  constructor(
+    img,
+    title,
+    description,
+    category,
+    price,
+    amount = 0,
+  ) {
+    this.id = ++Product.#count //унікальний id для товару
+    this.img = img
+    this.title = title
     this.description = description
     this.price = price
-    this.id = new Date().getTime()
+    this.category = category
+    this.amount = amount
+  }
+
+  static add = (...data) => {
+    const newProduct = new Product(...data)
+
+    this.#list.push(newProduct)
   }
 
   static getList = () => {
     return this.#list
   }
 
-  static add = (product) => {
-    return this.#list.push(product)
-  }
-
   static getById = (id) => {
     return this.#list.find((product) => product.id === id)
   }
 
-  static updateById = (id, { name }) => {
-    const product = this.getById(id)
-
-    if (product) {
-      if ({ name }) {
-        product.name = name
-      }
-      return true
-    } else {
-      return false
-    }
-  }
-
-  static deleteById = (id) => {
-    const index = this.#list.findIndex(
-      (product) => product.id === id,
+  static getRandomList = (id) => {
+    //фільтруємо товари, щоб вилучити той, який з яким порівнюємо id
+    const filteredList = this.#list.filter(
+      (product) => product.id !== id,
     )
 
-    if (index !== -1) {
-      this.#list.splice(index, 1)
+    //Відсортовуємо через Math.random() та перемішаємо масив
+    const shuffledList = filteredList.sort(
+      () => Math.random() - 0.5,
+    )
+
+    // Повертаємо перші 3 елементи з перемішеного масиву
+    return shuffledList.slice(0, 3)
+  }
+}
+
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/`,
+  `AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС`,
+  [
+    { id: 1, text: 'Готовий до відправки' },
+    { id: 2, text: 'Топ продажів' },
+  ],
+  27000,
+  10,
+)
+
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel`,
+  `Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux`,
+  [{ id: 2, text: 'Топ продажів' }],
+  20000,
+  10,
+)
+
+Product.add(
+  'https://picsum.photos/200/300',
+  `Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)`,
+  `Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС`,
+  [{ id: 1, text: 'Готовий до відправки' }],
+  25000,
+  10,
+)
+
+class Purchase {
+  static DELIVERY_PRICE = 150
+  static #count = 0
+  static #list = []
+
+  constructor(data, product) {
+    this.id = ++Purchase.#count
+
+    this.firstname = data.firstname
+    this.lastname = data.lastname
+
+    this.phone = data.phone
+    this.email = data.email
+
+    this.comment = data.comment || null
+    this.bonus = data.bonus || 0
+
+    this.promocode = data.promocode || null
+
+    this.totalPrice = data.totalPrice
+    this.productPrice = data.productPrice
+    this.deliveryPrice = data.deliveryPrice
+    this.amount = data.amount
+
+    this.product = product
+  }
+
+  static add = (...arg) => {
+    const newPurchase = new Purchase(...arg)
+
+    this.#list.push(newPurchase)
+
+    return newPurchase
+  }
+
+  static getList = () => {
+    return Purchase.#list.reverse()
+  }
+
+  static getById = (id) => {
+    return Purchase.#list.find((item) => item.id === id)
+  }
+
+  static updateById = (id, data) => {
+    const purchase = Purchase.getById(id)
+
+    if (purchase) {
+      if (data.firstname)
+        purchase.firstname = data.firstname
+      if (data.lastname) purchase.lastname = data.lastname
+      if (data.phone) purchase.phone = data.phone
+      if (data.email) purchase.email = data.email
+
       return true
     } else {
       return false
@@ -54,84 +147,181 @@ class Product {
 }
 
 // ================================================================
-// router.get Створює нам один ентпоїнт
-
-// ↙️ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  const list = Product.getList()
-
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('purchase-index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
+    style: 'purchase-index',
     data: {
-      products: {
-        list,
-        isEmpty: list.length === 0,
-      },
+      list: Product.getList(),
     },
   })
 })
-
 // ================================================================
-router.post('/product-create', function (req, res) {
+// ================================================================
+router.get('/purchase-product', function (req, res) {
+  const id = Number(req.query.id)
+
+  res.render('purchase-product', {
+    style: 'purchase-product',
+    data: {
+      list: Product.getRandomList(id),
+      product: Product.getById(id),
+    },
+  })
+})
+// ================================================================
+// ================================================================
+router.post('/purchase-create', function (req, res) {
+  const id = Number(req.query.id)
+  const amount = Number(req.body.amount)
+
+  if (amount < 1) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Некоректна кількість обраниого товару',
+        link: `/purchase-product?id=${id}`,
+      },
+    })
+  }
+
+  const product = Product.getById(id)
+
+  if (product.amount < 1) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Такої кількості товару немає в наявності',
+        link: `/purchase-product?id=${id}`,
+      },
+    })
+  }
+
+  console.log(product, amount)
+
+  const productPrice = product.price * amount
+  const totalPrice = productPrice + Purchase.DELIVERY_PRICE
+
+  res.render('purchase-create', {
+    style: 'purchase-create',
+    data: {
+      // list: Product.getRandomList(id),
+      // product: Product.getById(id),
+
+      id: product.id,
+      cart: [
+        {
+          text: `${product.title} (${amount}) шт.`,
+          price: productPrice,
+        },
+        {
+          text: `Доставка`,
+          price: Purchase.DELIVERY_PRICE,
+        },
+      ],
+      totalPrice,
+      productPrice,
+      deliveryPrice: Purchase.DELIVERY_PRICE,
+      amount,
+    },
+  })
+})
+// ================================================================
+// ================================================================
+router.post('/purchase-submit', function (req, res) {
   // console.log(req.body)
 
-  const { name, description, price } = req.body
+  const id = Number(req.query.id)
 
-  const newProduct = new Product(name, description, price)
+  let {
+    totalPrice,
+    productPrice,
+    deliveryPrice,
+    amount,
 
-  Product.add(newProduct)
-  // console.log(Product.getList())
+    firstname,
+    lastname,
+    email,
+    phone,
+  } = req.body
 
-  res.render('alert', {
-    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+  const product = Product.getById(id)
+
+  if (!product) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Товар не знайдено',
+        link: `/purchase-list`,
+      },
+    })
+  }
+
+  totalPrice = Number(totalPrice)
+  productPrice = Number(productPrice)
+  deliveryPrice = Number(deliveryPrice)
+  amount = Number(amount)
+
+  console.log('totalPrice:', totalPrice)
+  console.log('productPrice:', productPrice)
+  console.log('deliveryPrice:', deliveryPrice)
+  console.log('amount:', amount)
+
+  if (
+    isNaN(totalPrice) ||
+    isNaN(productPrice) ||
+    isNaN(deliveryPrice) ||
+    isNaN(amount)
+  ) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: 'Помилка',
+        info: 'Некоректні дані',
+        link: `/purchase-list`,
+      },
+    })
+  }
+
+  if (!firstname || !lastname || !email || !phone) {
+    return res.render('alert', {
+      style: 'alert',
+      data: {
+        message: "Заповніть обов'язкові поля",
+        info: 'Некоректні дані',
+        link: `/purchase-list`,
+      },
+    })
+  }
+
+  const purchase = Purchase.add(
+    {
+      totalPrice,
+      productPrice,
+      deliveryPrice,
+      amount,
+
+      firstname,
+      lastname,
+      email,
+      phone,
+    },
+    product,
+  )
+
+  console.log(purchase)
+
+  return res.render('alert', {
     style: 'alert',
-    data: {},
-    // info: result
-    //   ? 'Успішне виконання дії'
-    //   : 'Помилка: можливо ви ввели дані неправильно',
-  })
-})
-
-// ================================================================
-// router.get Створює нам один ентпоїнт
-
-router.get('/product-edit', function (req, res) {
-  const { id } = req.query
-
-  // console.log(typeof id)
-
-  Product.deleteById(Number(id))
-
-  res.render('product-edit', {
-    style: 'product-edit',
-  })
-})
-// ================================================================
-// ================================================================
-
-router.get('/product-delete', function (req, res) {
-  // console.log(req.body)
-
-  res.render('alert-delete', {
-    style: 'alert-delete',
-    info: 'Товар видалено',
-  })
-})
-// ================================================================
-// ================================================================
-
-router.post('/product-update', function (req, res) {
-  // console.log(req.body)
-
-  const { id, name, description, price } = req.body
-
-  console.log(id, name, description, price)
-
-  res.render('alert-update', {
-    style: 'alert-update',
-    info: 'Зміни товару були збережені',
+    data: {
+      message: 'Успішно',
+      info: 'Замовлення створено',
+      link: `/purchase-list`,
+    },
   })
 })
 // ================================================================
